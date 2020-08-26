922c8fcd0f033e913f26a2ae741be964531fe2ec
dione bigode (via Travis CI) <jamarson@gmail.com>
2020-08-26 22:53:21 +0000

    Deploy johnnybigoode/test-driven-carousel to github.com/johnnybigoode/test-driven-carousel.git:gh-pages

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: ['react'],
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    env: {
+        node: true,
+    },
+    rules: {
+        quotes: ['error', 'single', { avoidEscape: true }],
+        'comma-dangle': ['error', 'always-multiline'],
+        'no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
---
+++ src/tests/Carousel.test.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Carousel, { Carousel as CoreCarousel } from '../Carousel';
+import CarouselButton from '../CarouselButton';
+import CarouselSlide from '../CarouselSlide'
+
+describe('Carousel', () => {
+    const slides = [
+        {
+            imgUrl: 'http://example.com/slide1.png',
+            description: 'Slide 1',
+            attribution: 'Uno Pizzeria',
+        },
+        {
+            imgUrl: 'http://example.com/slide2.png',
+            description: 'Slide 2',
+            attribution: 'Dos Equis',
+        },
+        {
+            imgUrl: 'http://example.com/slide3.png',
+            description: 'Slide 3',
+            attribution: 'Three Amigos',
+        },
+    ];
+
+    describe('componente with HOC', () => {
+        let mounted;
+
+        beforeEach(() => {
+            mounted = mount(<Carousel slides={slides} />);
+        });
+
+        it('sets slideIndex={0} on the core component', () => {
+            expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
+        });
+
+        it('passes `slides` down to the core component', () => {
+            expect(mounted.find(CoreCarousel).prop('slides')).toBe(slides);
+        });
+
+        it('allows `slideIndex` to be controlled', () => {
+            const mounted = mount(<Carousel slides={slides} slideIndex={1} />);
+            expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(1);
+            mounted.setProps({ slideIndex: 0 });
+            expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
+        });
+
+        it('advances the slide after `autoAdvanceDelay` elapses', () => {
+            jest.useFakeTimers();
+            const autoAdvanceDelay = 10e3;
+            mounted = mount(
+                <Carousel slides={slides} autoAdvanceDelay={autoAdvanceDelay} />
+            );
+            jest.advanceTimersByTime(autoAdvanceDelay);
+            mounted.update();
+            expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(1);
+        });
+
+    });
+
+    describe('core component', () => {
+        const slideIndexDecrement = jest.fn();
+        const slideIndexIncrement = jest.fn();
+        let wrapper;
+
+        beforeEach(() => {
+            wrapper = shallow(
+                <CoreCarousel
+                    slides={slides}
+                    slideIndex={0}
+                    slideIndexDecrement={slideIndexDecrement}
+                    slideIndexIncrement={slideIndexIncrement}
+                />);
+        });
+
+        it('renders a <div>', () => {
+            expect(wrapper.type()).toBe('div');
+        });
+
+        it('has an initial `slideIndex of 0`', () => {
+            expect(wrapper.state('slideIndex')).toBe(0);
+        });
+
+        it('renders a CarouselButton labeled "Prev"', () => {
+            expect(
+                wrapper
+                    .find(CarouselButton)
+                    .at(0)
+                    .prop('children')
+            ).toBe('Prev');
+        });
+
+        it('renders a CarouselButton labeled "Next"', () => {
+            expect(
+                wrapper
+                    .find(CarouselButton)
+                    .at(1)
+                    .prop('children')
+            ).toBe('Next');
+        });
+
+        it('passes defaultImg and defaultImgHeight on the CarouselSLide', () => {
+            const defaultImg = () => 'test';
+            const defaultImgHeight = 1234;
+            wrapper.setProps({ defaultImg, defaultImgHeight });
+            expect(wrapper.find(CarouselSlide).prop('Img')).toBe(defaultImg);
+            expect(wrapper.find(CarouselSlide).prop('imgHeight')).toBe(defaultImgHeight);
+        });
+
+        it('allows individual slides to override Img and imgHeight', () => {
+            const Img = () => 'test';
+            const imgHeight = 1234;
+            wrapper.setProps({ slides: [{ ...slides[0], Img, imgHeight }] });
+            expect(wrapper.find(CarouselSlide).prop('Img')).toBe(Img);
+            expect(wrapper.find(CarouselSlide).prop('imgHeight')).toBe(imgHeight);
+        });
+
+        it('renders the current slide as a CarouselSlide', () => {
+            let slideProps;
+            slideProps = wrapper.find(CarouselSlide).props();
+            expect(slideProps).toEqual({
+                ...CarouselSlide.defaultProps,
+                ...slides[0],
+            });
+            wrapper.setProps({ slideIndex: 1 });
+            slideProps = wrapper.find(CarouselSlide).props();
+            expect(slideProps).toEqual({
+                ...CarouselSlide.defaultProps,
+                ...slides[1],
+            });
+        });
+
+        it('decrements `slideInex` when Prev is clicked', () => {
+            wrapper.find('[data-action="prev"]').simulate('click');
+            expect(slideIndexDecrement).toHaveBeenCalledWith(slides.length);
+        });
+
+        it('increments `slideInex` when Prev is clicked', () => {
+            wrapper.find('[data-action="next"]').simulate('click');
+            expect(slideIndexIncrement).toHaveBeenCalledWith(slides.length);
+        });
+    });
+});
